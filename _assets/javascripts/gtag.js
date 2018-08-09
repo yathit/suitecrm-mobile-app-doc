9614c65a25e0ce29e2ac9be65b22c67997e4e935
Kyaw Tun <kyawtun@yathit.com>
2018-08-09 21:26:32 +0800

    analytics

--- _assets/javascripts/gtag.js
+++ _assets/javascripts/gtag.js
@@ -3,3 +3,12 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-114586412-1');
+
+
+function sendAnalytic(action, category, label, value) {
+  gtag('event', action, {
+    'event_category': category,
+    'event_label': label,
+    'value': value
+  });
+}
