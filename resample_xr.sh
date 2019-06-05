c3a82c015bccfef3efb3c07b5b062e055a908cbd
Kyaw <kyawtun@yathit.com>
2019-06-05 15:59:56 +0800

    resempling

--- resample_xr.sh
+++ resample_xr.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 ## Resample iPhone XR screencast
 
-# extract frame as poster image
+# extract frame as poster image at 10 s
 # ffmpeg  -i check-restore-purchase.mov -vframes 1 -ss 00:10 check-restore-purchase.jpeg
 
-sips --resampleWidth 360 "$1.jpeg"
-ffmpeg -i "$1.mov" -an -vf  scale="360:780" "$1.mp4"
-ffmpeg -i "$1.mp4" -an "$1.webm"
+IN="$1"
+IN_MOV="$IN.mov"
+if [ -f $IN_MOV ] ; then
+    echo "$IN"
+else
+    echo "input file $IN_MOV not exist"
+    exit 1
+fi
 
-trash $1.mov
-gsutil mv $1.* gs://yathit-assets/screencast/sm/xr/
+FN_JPG="$IN.jpeg"
+if [ -f $FN_JPG ] ; then
+  echo "$FN_JPG"
+else
+  echo "Creating screenshot image $FN_JPG at 10 s"
+    ffmpeg  -i "$IN_MOV" -vframes 1 -ss 00:10 "$FN_JPG"
+fi
+
+sips --resampleWidth 360 "$FN_JPG"
+ffmpeg -i "$IN.mov" -an -vf  scale="360:780" "$IN.mp4"
+ffmpeg -i "$IN.mp4" -vcodec libvpx -cpu-used -5 -deadline realtime "$IN.webm"
+
+trash $IN.mov
+gsutil mv $IN.* gs://yathit-assets/screencast/sm/xr/
 echo https://yathit-assets.storage.googleapis.com/screencast/sm/xr/
